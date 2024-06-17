ac7207e6cece35c3ff23de4e48a628620a881777
Stefan Izota <1038308+stefanizota@users.noreply.github.com>
2024-06-17 16:22:13 +0300

    Finished example.

--- src/main.py
+++ src/main.py
@@ -1,10 +1,16 @@
-import json, requests, logging, web3, os, time, datetime
+import json, requests, logging, web3, os, time, datetime, first
 
 from ocean_lib import aquarius, ocean
 from ocean_lib.ocean.ocean import Ocean, OceanAssets
 from ocean_lib.ocean.util import to_wei
 from config import get_config_dict
 from eth_account import Account
+from ocean_lib.models.fixed_rate_exchange import (
+  ExchangeArguments,
+  FixedRateExchange,
+  OneExchange
+)
+from ocean_lib.ocean.util import get_address_of_type
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("consumer")
@@ -12,6 +18,8 @@ logger = logging.getLogger("consumer")
 NETWORK_RPC = os.environ["NETWORK_RPC"]
 AQUARIUS_URL = os.getenv("AQUARIUS_URL")
 SUBGRAPH_URL = os.getenv("SUBGRAPH_URL")
+MAX_GAS_PRICE = int(os.getenv("MAX_GAS_PRICE"))
+
 DID = os.getenv("DID")
 
 WALLET_PRIVATE_KEY = os.getenv("WALLET_PRIVATE_KEY")
@@ -23,11 +31,11 @@ OCEAN = ocean.OCEAN_token
 
 aquarius = aquarius.Aquarius(AQUARIUS_URL)
 asset = aquarius.get_ddo(DID)
-datatokenAddress = asset.datatokens[0]["address"]
-logger.info(datatokenAddress)
+datatoken_address = asset.datatokens[0]["address"]
+logger.info({'asset': asset.as_dictionary()})
 
-logger.info("----")
-
+# Find the exchange id in order to swap base token to dataset token
+# TODO: use server filter
 query = """
 {
   fixedRateExchanges(subgraphError:deny){
@@ -67,17 +75,29 @@ query = """
 headers = {"Content-Type": "application/json"}
 payload = json.dumps({"query": query})
 response = requests.request("POST", SUBGRAPH_URL, headers=headers, data=payload)
-fixedRateExchanges = json.loads(response.text)
+fre_infos = json.loads(response.text)
 
-exchangeDict = None
-for fixedRateExchange in fixedRateExchanges["data"]["fixedRateExchanges"]:
-    logger.info(fixedRateExchange['datatoken']['id'].lower())
-    if fixedRateExchange['datatoken']['id'].lower() == datatokenAddress.lower():
-        exchangeDict = fixedRateExchange
-        break
-logger.info(exchangeDict)
+one_exchange_info = first.first(
+  fre_infos["data"]["fixedRateExchanges"],
+  key=lambda fre_info: fre_info['datatoken']['id'].lower() == datatoken_address.lower())
+logger.info({'exchange_info': one_exchange_info})
 
-#OCEAN.approve(ocean.fixed_rate_exchange.address, 150, {"from": consumer})
+fre_addr = get_address_of_type(config, "FixedPrice")
+fre = FixedRateExchange(config, fre_addr)
+logger.info({'fixed_rate_exchange': fre})
 
-# create exchange from exchange dict
-# exchange.buy_DT(to_wei(1), consume_market_fee=0, tx_dict={"from": consumer})
+one_exchange = OneExchange(fre, exchange_id=one_exchange_info['exchangeId'])
+logger.info({'one_exchange': one_exchange})
+
+# Approve tokens
+tokens_needed = one_exchange.BT_needed(to_wei(1), consume_market_fee=0)
+ocean.OCEAN_token.approve(one_exchange.address, tokens_needed, {"from": consumer, "gasPrice": MAX_GAS_PRICE})
+
+# Buy data token
+one_exchange.buy_DT(to_wei(1), consume_market_fee=0, tx_dict={"from": consumer, "gasPrice": MAX_GAS_PRICE})
+
+# Submit order
+order_tx_id = ocean.assets.pay_for_access_service(asset, {"from": consumer, "gasPrice": MAX_GAS_PRICE})
+
+# Download the dataset
+asset_dir = ocean.assets.download_asset(asset, consumer, './', order_tx_id)
